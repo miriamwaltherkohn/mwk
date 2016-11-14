448756fd52c26f5f4296540ea6002d2b91ee2cb8
Clara <clara.vuillemin@posteo.de>
2016-11-14 14:23:39 +0100

    general bug and logic fix

--- luv2.py
+++ luv2.py
@@ -1,18 +1,23 @@
-    print "Hello LUV!"
+print "Hello LUV!"
 
 potential_content = raw_input("Name one of your urgencies?")
 potential_collegues = raw_input("Who shares this urgency with you?")
 potential_methode = raw_input("How do you want to approach this shared urgency?")
 
 if len(potential_content) > 0 and potential_content.isalpha():
-if len(potential_collegues) > 0 and potential_collegues.isalpha():
-if len(potential_methode) > 0 and potential_methode.isalpha():
-    print "Imagine yourself far away on top of a hill inside and around a castle with %s, envisioning the future of %s, while %s." % (potential_collegues,potential_content,potential_methode)
+    if len(potential_collegues) > 0 and potential_collegues.isalpha():
+        if len(potential_methode) > 0 and potential_methode.isalpha():
+            print "Imagine yourself far away on top of a hill inside and around a castle with %s, envisioning the future of %s, while %s." % (potential_collegues,potential_content,potential_methode)
 #It may sound creepy, but trust me it's not. ;-)
 else:
-    print "Try again"
+    print "Try again"    
 
-def luv_approach(f,u)
+number_of_recurring_folks = raw_input("How many people you already know participate in luv17?")
+number_of_recurring_folks = int(number_of_recurring_folks)
+number_of_shared_urgencies = raw_input("How many common topics do you excpect at luv17?")
+number_of_shared_urgencies = int(number_of_shared_urgencies)
+
+def luv_approach(f,u):
     # f = number of recurring folks
     # u = number of shared urgencies
     if f >= 20 and u == 1:
@@ -22,4 +27,4 @@ def luv_approach(f,u)
     else:
         return "The werewolves are watching you!"
 
-luv_approach() #add your arguments here!
+print luv_approach(number_of_recurring_folks, number_of_shared_urgencies) #add your arguments here!
